822db04600f1c5e5b1cc9dadc4eae16d0f0f8120
NachoDelfiner <delfinerignacio@gmail.com>
2023-06-05 16:58:50 -0300

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./components/Home/Home";
+import { Card } from "./components/Card/Card";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/pokemon/:id",
+    element: <Card />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
